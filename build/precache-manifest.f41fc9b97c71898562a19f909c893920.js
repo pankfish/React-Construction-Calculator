bbfaf61da1240e0b50dc6643ebe5fdc29547a45f
pankfish <pankfish@penguin>
2019-09-10 21:41:17 -0500

    build

---
+++ build/precache-manifest.f41fc9b97c71898562a19f909c893920.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6a4e796f07351e833518d8f091a8e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e03e9f74c55a41ad60fe",
+    "url": "/static/css/main.688068d5.chunk.css"
+  },
+  {
+    "revision": "08899087ce88177a8e4a",
+    "url": "/static/js/2.b62a53a5.chunk.js"
+  },
+  {
+    "revision": "e03e9f74c55a41ad60fe",
+    "url": "/static/js/main.97148214.chunk.js"
+  },
+  {
+    "revision": "f03679e9f8f195a28d1d",
+    "url": "/static/js/runtime~main.4f2167ab.js"
+  }
+]);
